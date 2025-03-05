be27c3e9830c74dd4e784508f08eb974c2aa92d2
Matt Everett <matt.everett@toyotaconnected.com>
2025-03-05 10:05:47 -0500

    path

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../automate/automate-git.py --download-dir=code/chromium_git --depot-tools-dir=code/depot_tools --no-distrib --no-build
+python3 code/automate/automate-git.py --download-dir=code/chromium_git --depot-tools-dir=code/depot_tools --no-distrib --no-build
