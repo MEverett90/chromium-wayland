fe26deb62c43dd76ae0f4458dea899cbed6e626b
Matt Everett <matt.everett@toyotaconnected.com>
2025-03-05 09:53:23 -0500

    Fix paths

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../automate/automate-git.py --download-dir=/home/marshall/code/chromium_git --depot-tools-dir=/home/marshall/code/depot_tools --no-distrib --no-build
+python3 ../automate/automate-git.py --download-dir=code/chromium_git --depot-tools-dir=code/depot_tools --no-distrib --no-build
